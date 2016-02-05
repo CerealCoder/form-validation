a7d76470f66bbf66b644855a71ed534b5eda530a
MilkshakePanda <iam.superpeluche@gmail.com>
2016-02-05 12:54:59 +0100

    form validation part 1

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,65 @@
-import {test} from './lib/module';
+let userCardInput = document.getElementById('cardNumber')
+let cvcInput = document.getElementById('cvc')
+let nameInput = document.getElementById('cardName')
+let emailInput = document.getElementById('email')
+let submitBtn = document.querySelector('.payment-form__submit')
 
-test.log()
+let digitKeyCodes = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 37, 39, 8]
+let pattern = /(\d{4})/g
+let whitespaces = /\s/g
+
+
+
+let validateEmail = function() {
+
+
+    let pattern = /([a-zA-Z0-9\-\.])+@([a-zA-Z0-9\-\.])+([a-zA-Z.])+$/g
+
+    if (emailInput.value.match(pattern)) {
+        console.log("valid email");
+    } else {
+        alert("yoooo check the email bro")
+    }
+
+}
+
+let validateName = function(event) {
+    let pattern = /[a-zA-Z'\s -]/g
+    let keyPressed = event.keyCode
+    keyPressed = String.fromCharCode(keyPressed)
+    if (!keyPressed.match(pattern)) {
+        event.preventDefault()
+    }
+}
+
+let blockNonDigits = function(event) {
+
+    if (digitKeyCodes.indexOf(event.keyCode) === -1) {
+        console.log("not in the array");
+        event.preventDefault()
+    }
+
+}
+
+
+userCardInput.addEventListener('blur', function() {
+
+    if (!this.value.match(whitespaces)) {
+        this.value = this.value.replace(pattern, "$1 ").trim()
+    }
+
+}, false)
+userCardInput.addEventListener('keydown', function(event) {
+
+    blockNonDigits(event)
+
+}, false)
+cvcInput.addEventListener('keydown', function(event) {
+
+    blockNonDigits(event)
+
+}, false)
+nameInput.addEventListener('keypress', function(event) {
+    validateName(event);
+})
+submitBtn.addEventListener('click', validateEmail, false)
---
+++ notes.md
@@ -0,0 +1,8 @@
+## Notes
+
+
+- Email RegX: Any Letter or Digit is allowed in the input as well as "." and underscores (no commas, no hyphens) it can be of any length
+- Same for what comes after the @ symbol (any length is allowed)
+- It has to start with a non special character
+- After the domain name, check for the dot symbol, the extension has to be only characters (uppercase or lowercase) and limited to 7.
+- It can contain dots (co.uk) no special character
